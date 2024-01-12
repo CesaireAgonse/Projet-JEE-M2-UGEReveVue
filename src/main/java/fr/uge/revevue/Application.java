6b3743704bc95e8109da6d244ead78e13b692845
Vincent <vincentperezlefeuvre@gmail.com>
2024-01-13 00:45:46 +0100

    implémentation de l'entity User

--- src/main/java/fr/uge/revevue/Application.java
+++ src/main/java/fr/uge/revevue/Application.java
@@ -1,7 +1,10 @@
-package fr.uge;
+package fr.uge.revevue;
 
+import fr.uge.revevue.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class Application {
