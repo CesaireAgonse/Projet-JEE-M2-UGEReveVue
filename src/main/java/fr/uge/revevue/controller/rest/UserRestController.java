6c2b3f30f438287c86f986412faca0345159142f
Nico <harrylamagie@icloud.com>
2024-02-12 15:22:21 +0100

    Implémentation du UserRestController pour le PostMapping du password, le GetMapping des UserInformation et les PostMapping de follow unfollow

--- src/main/java/fr/uge/revevue/controller/rest/UserRestController.java
+++ src/main/java/fr/uge/revevue/controller/rest/UserRestController.java
@@ -1,6 +1,7 @@
 package fr.uge.revevue.controller.rest;
 
 import fr.uge.revevue.form.LoginForm;
+import fr.uge.revevue.form.PasswordForm;
 import fr.uge.revevue.information.AuthenticationInformation;
 import fr.uge.revevue.information.SimpleUserInformation;
 import fr.uge.revevue.information.UserInformation;
@@ -12,6 +13,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,4 +34,42 @@ public class UserRestController {
         userService.delete(userId);
         return ResponseEntity.noContent().build();
     }
+
+
+    @PostMapping("/password")
+    public ResponseEntity<Void> password(@RequestBody @Valid PasswordForm passwordForm, BindingResult result){
+        if (result.hasErrors()){
+            return  ResponseEntity.badRequest().build();
+        }
+        var userInformation = userService.modifyPassword(passwordForm.getCurrentPassword(), passwordForm.getNewPassword());
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<UserInformation> information(@PathVariable String username){
+        var userInformation = userService.getInformation(username);
+        if (userInformation == null){
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(userInformation);
+    }
+
+    @PostMapping("/follow/{username}")
+    public ResponseEntity<Void> follow(@PathVariable String username){
+        if (!userService.isExisted(username)){
+            return ResponseEntity.badRequest().build();
+        }
+        userService.follow(userService.currentUser().getUsername(), username);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/unfollow/{username}")
+    public ResponseEntity<Void> unfollow(@PathVariable String username){
+        if (!userService.isExisted(username)){
+            return ResponseEntity.badRequest().build();
+        }
+        userService.unfollow(userService.currentUser().getUsername(), username);
+        return ResponseEntity.noContent().build();
+    }
+
 }
